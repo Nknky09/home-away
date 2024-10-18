7b86bddbf5567b60b7bf28e7047245f3f2d2262d
Nknky09 <ks.shido@gmail.com>
2024-10-18 00:18:44 -0400

    Ver3.91 Finish supabase image upload Schema prisma for database classes Start creating create rentals page form

--- app/utils/actions.ts
+++ app/utils/actions.ts
@@ -1,11 +1,18 @@
 "use server";
 
-import { imageSchema, profileSchema, validateWithZodSchema } from "./schemas";
+import {
+  imageSchema,
+  profileSchema,
+  propertySchema,
+  validateWithZodSchema,
+} from "./schemas";
 import db from "./db";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { error } from "console";
+import { uploadImage } from "./supabase";
+import { render } from "react-dom";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -111,8 +118,38 @@ export const updateProfileImageAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  const image = formData.get("image") as File;
-  const validatedFields = validateWithZodSchema(imageSchema, { image });
-  console.log(validatedFields);
-  return { message: "Profile image updated successfully" };
+  const user = await getAuthUser();
+  try {
+    const image = formData.get("image") as File;
+    const validatedFields = validateWithZodSchema(imageSchema, { image });
+    const fullPath = await uploadImage(validatedFields.image);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: {
+        profileImage: fullPath,
+      },
+    });
+    revalidatePath("/profile");
+    return { message: "Profile image updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
+
+export const createPropertyAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect("/");
 };
