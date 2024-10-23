6e0e88e764716c14331e25f32aeacfe51ae7579e
Nknky09 <ks.shido@gmail.com>
2024-10-23 00:13:37 -0400

    Ver3.115 Property Details Page npm i react-share share buttons Bread crumbs

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,5 +1,15 @@
-function FavoritesPage() {
-  return <h1 className="text-3xl">FavoritesPage</h1>;
+import EmptyList from "@/components/home/EmptyList";
+import { fetchFavorites } from "../utils/actions";
+import PropertiesList from "@/components/home/PropertiesList";
+
+async function FavoritesPage() {
+  const favorites = await fetchFavorites();
+
+  if (favorites.length === 0) {
+    return <EmptyList />;
+  }
+
+  return <PropertiesList properties={favorites} />;
 }
 
 export default FavoritesPage;
---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,43 @@
+import { fetchPropertyDetails } from "@/app/utils/actions";
+import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
+import PropertyRating from "@/components/card/PropertyRating";
+import BreadCrumbs from "@/components/properties/BreadCrumbs";
+import ImageConatiner from "@/components/properties/ImageConatiner";
+import ShareButton from "@/components/properties/ShareButton";
+import { redirect } from "next/navigation";
+
+async function PropertiesDetailsPage({ params }: { params: { id: string } }) {
+  const property = await fetchPropertyDetails(params.id);
+  if (!property) redirect("/");
+  const { baths, bedrooms, beds, guests } = property;
+  const details = { baths, bedrooms, beds, guests };
+
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold capitalize">{property.tagline}</h1>
+        <div className="flex items-center gap-x-4">
+          {/*share button*/}
+          <ShareButton name={property.name} propertyId={property.id} />
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+      <ImageConatiner mainImage={property.image} name={property.name} />
+
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{property.name}</h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center">
+          {/* calendar */}
+        </div>
+      </section>
+    </section>
+  );
+}
+
+export default PropertiesDetailsPage;
