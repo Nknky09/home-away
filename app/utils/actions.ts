d8fc87eccb2e6d38222cc46cc123426ae2bd86f2
Nknky09 <ks.shido@gmail.com>
2024-10-23 22:13:10 -0400

    Vercel deployment - more error fixes

--- app/utils/actions.ts
+++ app/utils/actions.ts
@@ -89,6 +89,7 @@ export const fetchProfile = async () => {
 };
 
 export const updateProfileAction = async (
+  prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
   const user = await getAuthUser();
