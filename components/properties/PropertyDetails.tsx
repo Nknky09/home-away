b524dbffa65335043c19e97495223530d0652fcc
Nknky09 <ks.shido@gmail.com>
2024-10-25 12:42:20 -0400

    fix properties[id] page to show PropetiesDetails properly
    tested on local
    upload and launch on Vercel

--- components/properties/PropertyDetails.tsx
+++ components/properties/PropertyDetails.tsx
@@ -14,9 +14,9 @@ function PropertiesDetails({
 }: PropertyDetailsProps) {
   return (
     <p className="text-md font-light">
-      <span>{formatQuantity(bedrooms, "bedroom")} &middot;</span>
-      <span>{formatQuantity(baths, "bath")} &middot;</span>
-      <span>{formatQuantity(guests, "guest")} &middot;</span>
+      <span>{formatQuantity(bedrooms, "bedroom")} </span>
+      <span>{formatQuantity(baths, "bath")} </span>
+      <span>{formatQuantity(guests, "guest")} </span>
       <span>{formatQuantity(beds, "bed")}</span>
     </p>
   );
