89f5f662e1ed74ff0629667a92a47fdadc6504b6
Nknky09 <ks.shido@gmail.com>
2024-10-27 22:10:31 -0400

    review, check if existing user, or user has already submitted, or user is property owner. Start Booking, add booking calls to schema.prisma

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { fetchPropertyDetails } from "@/app/utils/actions";
+import { fetchPropertyDetails, findExistingReview } from "@/app/utils/actions";
 import { redirect } from "next/navigation";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
@@ -15,6 +15,7 @@ import dynamic from "next/dynamic";
 import { Skeleton } from "@/components/ui/skeleton";
 import SubmitReview from "@/components/reviews/SubmitReview";
 import PropertyReviews from "@/components/reviews/PropertyReviews";
+import { auth } from "@clerk/nextjs/server";
 
 const DynamicMap = dynamic(
   () => import("@/components/properties/PropertyMap"),
@@ -31,6 +32,12 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const details = { baths, bedrooms, beds, guests };
   const firstName = property.profile.firstName;
   const profileImage = property.profile.profileImage;
+
+  const { userId } = auth();
+  const isNotOwner = property.profile.clerkId !== userId;
+  const reviewDoesNotExist =
+    userId && isNotOwner && !(await findExistingReview(userId, property.id));
+
   return (
     <section>
       <BreadCrumbs name={property.name} />
@@ -61,7 +68,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <BookingCalendar />
         </div>
       </section>
-      <SubmitReview propertyId={property.id} />
+      {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
       <PropertyReviews propertyId={property.id} />
     </section>
   );
