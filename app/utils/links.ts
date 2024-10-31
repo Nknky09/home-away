128ba2440993ffb69db4e4d300632cb5f51251b3
Nknky09 <ks.shido@gmail.com>
2024-10-31 18:14:01 -0400

    Ver4.156 Reservations

--- app/utils/links.ts
+++ app/utils/links.ts
@@ -7,8 +7,10 @@ export const links: NavLink[] = [
   { href: "/", label: "home" },
   { href: "/favorites ", label: "favorites" },
   { href: "/bookings ", label: "bookings" },
-  { href: "/reviews", label: "reviews" },
+  { href: "/reviews ", label: "reviews" },
+  { href: "/reservations ", label: "reservations" },
   { href: "/rentals/create ", label: "create rental" },
-  { href: "rentals", label: "my rentals" },
+  { href: "/rentals ", label: "my rentals" },
+  { href: "/admin", label: "admin" },
   { href: "/profile ", label: "profile" },
 ];
